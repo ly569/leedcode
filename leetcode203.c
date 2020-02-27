ed73d8a975d73fc371cc48180bf5b29aa1b65281
ly569 <764270856@qq.com>
2020-02-27 17:48:41 +0800

    leetcode

---
+++ leetcode203.c
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* removeElements(struct ListNode* head, int val){
+    struct ListNode ret, *p = &ret, *q;
+    ret.next = head;
+    while (p && p->next) {
+        if (p->next->val == val) {
+            q = p->next;
+            p->next = p->next->next;
+            free(q);
+        } else {
+            p = p->next;
+        }
+    }
+    return ret.next;
+}
