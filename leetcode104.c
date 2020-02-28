f414f9490cf03ba4ef7f2784ff86372f7a5f6010
ly569 <764270856@qq.com>
2020-02-28 17:35:18 +0800

    leetcode

---
+++ leetcode104.c
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL) return 0;
+    int d1 = maxDepth(root->left);
+    int d2 = maxDepth(root->right);
+    return fmax(d1, d2) + 1;
+}
---
+++ leetcode110.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL) return 0;
+    int d1 = maxDepth(root->left);
+    int d2 = maxDepth(root->right);
+    if (d1 == -2 || d2 == -2 || abs(d1 - d2) > 1) return -2;
+    return fmax(d1, d2) + 1;
+}
+
+bool isBalanced(struct TreeNode* root) {
+    return maxDepth(root) >= 0;
+}
