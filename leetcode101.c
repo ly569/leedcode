4b0ccc76974431f53b2267d7197081900d93e91e
ly569 <58296803+ly569@users.noreply.github.com>
2020-08-04 15:20:03 +0800

    对称二叉树

--- leetcode101.c
+++ leetcode101.c
@@ -8,14 +8,14 @@
  */
 
 
-bool isSameTree(struct TreeNode* p, struct TreeNode *q){
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
     if (p == NULL && q == NULL) return true;
     if (p == NULL || q == NULL) return false;
     if (p->val - q->val) return false;
     return isSameTree(p->left, q->right) && isSameTree(p->right, q->left);
 }
 
-bool isSymmetric(struct TreeNode *root) {
+bool isSymmetric(struct TreeNode* root) {
     if (root == NULL) return true;
     return isSameTree(root->left, root->right);
 }
